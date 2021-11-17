b3c5ec96933eca7f49f10eb060803161b3e98d8e
Rudy De Busscher <rdebusscher@gmail.com>
2021-11-17 19:03:57 +0100

    Jakarta EE 9 getting Started - CDI

---
+++ CDI/src/main/java/fish/payara/jakarta/ee9/start/PersonCache.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) [2021] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fish.payara.jakarta.ee9.start;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class PersonCache {
+
+    private List<Person> allPersons = new ArrayList<>();
+
+    public List<Person> getAllPersons() {
+        return allPersons;
+    }
+
+    public void addPerson(@Observes AddPersonEvent addPersonEvent) {
+        allPersons.add(addPersonEvent.getPerson());
+    }
+}
